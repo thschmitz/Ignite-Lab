86fbb50a32fb4695eba76965c0bc4f9353cdd14e
thschmitz <thomas.henrique.schmitz@gmail.com>
2022-12-13 13:50:25 -0300

    Inversao de Dependencia

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+// import { AppService } from './app.service';
+import { MailService } from './mail/mail.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {} // Inversao de dependencia, ele nao busca informacoes, mas recebe
+  constructor(private readonly mailService: MailService) {} // Inversao de dependencia, ele nao busca informacoes, mas recebe
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return this.mailService.sendEmail();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { HttpModule } from './http.module';
+import { MailService } from './mail/mail.service';
+import { PostmarkMail } from './mail/postmark-mail.service';
+import { SMTPMail } from './mail/smtp-mail.service';
 
+// Acomplador de servicos e de controladores
 @Module({
-  imports: [],
+  imports: [HttpModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: MailService,
+      useClass: PostmarkMail,
+    },
+  ], // Services
 })
 export class AppModule {}
+
+// Injecao de dependencia -> Esta injetando dentro do AppController, nao estou passando para ele em momento nenhum.
